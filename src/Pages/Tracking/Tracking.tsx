a1bdc52a42744a52782b3a137d6790fdd5b11f81
Parker <parkerphillips2003@gmail.com>
2025-03-20 19:55:37 -0700

    Updated tracking page styling

--- src/Pages/Tracking/Tracking.tsx
+++ src/Pages/Tracking/Tracking.tsx
@@ -4,6 +4,7 @@ import { useEffect, useState } from "react";
 import { shortenedUrlsType } from "../Home/Components/UrlInfo/UrlInfo";
 import { DisplayClickCount } from "./Components/DisplayClickCount/DisplayClickCount";
 import { processData } from "../../Util/dataProcessor";
+import { DisplayRegionData } from "./Components/DisplayRegionData/DisplayRegionData";
 
 export type UrlClickType = {
   clickedAt: Date;
@@ -45,19 +46,91 @@ export const Tracking = () => {
       <h2 className={styles.pageClicksTitle}>
         Total Page Clicks: {urlData[0]?.urlMapping?.totalClicks || 0}
       </h2>
-      <div>
-        <button onClick={() => setTimeFrame("monthly")}>Monthly</button>
-        <button onClick={() => setTimeFrame("weekly")}>Weekly</button>
-        <button onClick={() => setTimeFrame("daily")}>Daily</button>
-        <button onClick={() => setTimeFrame("hourly")}>Hourly</button>
-        <button onClick={() => setTimeFrame("recent")}>Last Hour</button>
+
+      <div className={styles.dataDisplayWrapper}>
+        <div className={styles.dataWrapper}>
+          <DataButtons setTimeFrame={setTimeFrame} />
+          <div className={styles.chartsWrapper}>
+            <div className={styles.chartRow}>
+              <div className={styles.chartWrapper}>
+                <DisplayClickCount
+                  urlData={urlData}
+                  timeFrame={timeFrame}
+                  startDay={startDay}
+                  endDay={endDay}
+                />
+              </div>
+              <div className={styles.chartWrapper}>
+                <DisplayRegionData urls={urlData} />
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
-      <DisplayClickCount
-        urlData={urlData}
-        timeFrame={timeFrame}
-        startDay={startDay}
-        endDay={endDay}
-      />
+    </div>
+  );
+};
+
+type DataButtonsProps = {
+  setTimeFrame: React.Dispatch<React.SetStateAction<string>>;
+};
+
+const DataButtons = ({ setTimeFrame }: DataButtonsProps) => {
+  const [activeBtn, setActiveBtn] = useState(0);
+
+  const handleClick = (index: number) => {
+    switch (index) {
+      case 0:
+        setTimeFrame("monthly");
+        break;
+      case 1:
+        setTimeFrame("weekly");
+        break;
+      case 2:
+        setTimeFrame("daily");
+        break;
+      case 3:
+        setTimeFrame("hourly");
+        break;
+      case 4:
+        setTimeFrame("recent");
+        break;
+    }
+    setActiveBtn(index);
+  };
+
+  return (
+    <div className={styles.dataBtnsWrapper}>
+      <button
+        className={`${styles.dataBtn} ${activeBtn === 0 ? styles.active : ""}`}
+        onClick={() => handleClick(0)}
+      >
+        Monthly
+      </button>
+      <button
+        className={`${styles.dataBtn} ${activeBtn === 1 ? styles.active : ""}`}
+        onClick={() => handleClick(1)}
+      >
+        Weekly
+      </button>
+      <button
+        className={`${styles.dataBtn} ${activeBtn === 2 ? styles.active : ""}`}
+        onClick={() => handleClick(2)}
+      >
+        Daily
+      </button>
+      <button
+        className={`${styles.dataBtn} ${activeBtn === 3 ? styles.active : ""}`}
+        onClick={() => handleClick(3)}
+      >
+        Hourly
+      </button>
+      <button
+        className={`${styles.dataBtn} ${activeBtn === 4 ? styles.active : ""}`}
+        onClick={() => handleClick(4)}
+      >
+        Last Hour
+      </button>
     </div>
   );
 };
